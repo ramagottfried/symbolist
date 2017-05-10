0a0340b5e65f1c7b6affb20d99f5c49eac8124b3
Jean Bresson <bresson@ircam.fr>
2017-05-10 12:16:20 +0200

    initial source import

---
+++ Source/symbolist.cpp
@@ -0,0 +1,28 @@
+/***************************************************/
+/*!
+ *  @file       symbolist.cpp
+ *  @brief      Laibrary for symbolic score display and editing
+ *  @author     Jean Bresson & Rama Gottfried
+ *  @date       10/05/2017
+ */
+/***************************************************/
+#include "symbolist.hpp"
+
+
+int symbolistInit() {
+    return 1; // went ok
+};
+
+int symbolistExit() {
+    return 1; // went ok
+}
+
+const char* symbolistInfo() {
+    return "symbolist v.0.1";
+}
+
+
+
+
+
+
---
+++ Source/symbolist.hpp
@@ -0,0 +1,33 @@
+/************************************************************************************/
+/*!
+ *  @file       symbolist.hpp
+ *  @brief      Laibrary for symbolic score display and editing
+ *  @author     Jean Bresson & Rama Gottfried
+ *  @date       10/05/2017
+ *
+ */
+/************************************************************************************/
+#ifndef symbolist_hpp
+#define symbolist_hpp
+
+#ifdef _MSC_VER
+	#define SYMBOLIST_VISIBILITY_DEFAULT	__declspec( dllexport )
+#else
+	#define SYMBOLIST_VISIBILITY_DEFAULT	__attribute__ ((visibility ("default")))
+#endif
+
+#ifdef __cplusplus
+#define SYMBOLIST_C_EXPORTS   extern "C"
+#else
+#define SYMBOLIST_C_EXPORTS
+#endif
+
+#define SYMBOLIST_API SYMBOLIST_C_EXPORTS SYMBOLIST_VISIBILITY_DEFAULT
+
+
+SYMBOLIST_API int symbolistInit();
+SYMBOLIST_API int symbolistExit();
+SYMBOLIST_API const char* symbolistInfo();
+
+
+#endif /* symbolist_hpp */

60ac4f918c332b60428581914934c3be0e085200
Jean Bresson <bresson@ircam.fr>
2017-05-12 18:39:18 +0200

    prevent crash at closing lib window

--- Source/MainWindow.cpp
+++ Source/MainWindow.cpp
@@ -23,11 +23,13 @@ MainWindow::~MainWindow()
 {}
 
 
+
 void MainWindow::closeButtonPressed()
 {
         // This is called when the user tries to close this window. Here, we'll just
         // ask the app to quit when this happens, but you can change this to do
         // whatever you need.
-        JUCEApplication::getInstance()->systemRequestedQuit();
+        //JUCEApplication::getInstance()->systemRequestedQuit();
 }
+
 

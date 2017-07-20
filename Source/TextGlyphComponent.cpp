f4d92b918fd583fe6ba0a702169d657f48295d16
ramagottfried <rama.gottfried@gmail.com>
2017-07-20 08:00:47 -0400

    improved text resizing after editing

--- Source/TextGlyphComponent.cpp
+++ Source/TextGlyphComponent.cpp
@@ -1,2 +1,101 @@
 
 #include "TextGlyphComponent.h"
+
+void EditableTextObjListener::labelTextChanged (Label* l)
+{
+    cout << l->getText() << endl;
+    owner->updateText( l->getText() );
+    owner->resized();
+}
+
+EditableTextObj::EditableTextObj(BaseComponent *c) : Label( String(), String() ), owner(c)
+{
+    setEditable (false, true, false);
+    setJustificationType (Justification::centredLeft);
+    setBorderSize( BorderSize<int>(0,0,0,0) );
+    
+    listener = new EditableTextObjListener((TextGlphComponent*)c);
+    addListener(listener);
+    
+}
+    
+void EditableTextObj::paint (Graphics& g)
+{
+    owner->paint(g);
+    setColour( Label::textColourId, owner->getCurrentColor() );
+    Label::paint(g);
+}
+
+void EditableTextObj::mouseMove( const MouseEvent& event )
+{
+    owner->mouseMove(event);
+    Label::mouseMove(event);
+}
+
+void EditableTextObj::mouseDown( const MouseEvent& event )
+{
+    owner->mouseDown(event);
+    Label::mouseDown(event);
+}
+
+void EditableTextObj::mouseDrag( const MouseEvent& event )
+{
+    owner->mouseDrag(event);
+    Label::mouseDrag(event);
+    
+}
+
+void EditableTextObj::mouseUp( const MouseEvent& event )
+{
+    owner->mouseUp(event);
+    Label::mouseUp(event);
+    
+}
+
+//==========================================================================================
+
+TextGlphComponent::TextGlphComponent()
+{
+    textobj = new EditableTextObj(this);
+    textobj->setText (m_text, sendNotificationSync );
+    textobj->setColour( Label::textColourId, getCurrentColor() );
+    textobj->setColour( Label::backgroundColourId, Colours::transparentWhite );
+    textobj->setMinimumHorizontalScale(1.0f);
+    addAndMakeVisible(textobj);
+    
+}
+    
+TextGlphComponent::~TextGlphComponent(){
+    cout << "deleting text " <<  this << endl;
+};
+
+void TextGlphComponent::setBoundsFromSymbol( float x, float y , float w , float h)
+{
+    //setBounds( x, y - (h * 0.5), w , h);
+    textobj->setFont( Font(h) );
+    setBounds( x, y, textobj->getFont().getStringWidth(m_text), h );
+    textobj->setBounds( getLocalBounds() );
+}
+
+Rectangle<float> TextGlphComponent::symbol_export_bounds()
+{
+    auto b = getBounds().toFloat();
+    return Rectangle<float>( b.getX(), b.getY() + b.getHeight()/2, b.getWidth(), b.getHeight() );
+}
+
+void TextGlphComponent::importTextFromSymbol( const Symbol& s ) {}
+
+void TextGlphComponent::resized()
+{
+    BaseComponent::resized();
+    int h = getHeight();
+    setBounds(getX(), getY(), textobj->getFont().getStringWidth(m_text), h );
+    
+    textobj->setFont( Font( h ) );
+    textobj->setBounds( getLocalBounds() );
+}
+
+void TextGlphComponent::updateText( String str)
+{
+    m_text = str;
+}
--- Source/TextGlyphComponent.h
+++ Source/TextGlyphComponent.h
@@ -6,55 +6,33 @@
 // Use Label for creating text editor on click
 using namespace std;
 
+class TextGlphComponent;
+
+class EditableTextObjListener : public Label::Listener
+{
+public:
+    EditableTextObjListener(TextGlphComponent* c) : owner(c) {}
+    virtual void labelTextChanged (Label* l) override;
+    
+private:
+    TextGlphComponent*    owner;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (EditableTextObjListener)
+};
+
+
 class EditableTextObj : public Label
 {
 public:
-    EditableTextObj(BaseComponent *c) :
-    Label( String(), String() ),
-    owner(c)
-    {
-        setEditable (false, true, false);
-        setJustificationType (Justification::centredLeft);
-        setBorderSize( BorderSize<int>(0,0,0,0) );
-    }
-    
-    void paint (Graphics& g) override
-    {
-        owner->paint(g);
-        setColour( Label::textColourId, owner->getCurrentColor() );
-        Label::paint(g);
-    }
-        
-    void mouseMove( const MouseEvent& event ) override
-    {
-        owner->mouseMove(event);
-        Label::mouseMove(event);
-    }
-    
-    void mouseDown( const MouseEvent& event ) override
-    {
-        owner->mouseDown(event);
-        Label::mouseDown(event);
-    }
-
-    void mouseDrag( const MouseEvent& event ) override
-    {
-        owner->mouseDrag(event);
-        Label::mouseDrag(event);
-
-    }
-
-    void mouseUp( const MouseEvent& event ) override
-    {
-        owner->mouseUp(event);
-        Label::mouseUp(event);
-
-    }
-        
+    EditableTextObj(BaseComponent *c);
+    void paint (Graphics& g) override;
+    void mouseMove( const MouseEvent& event ) override;
+    void mouseDown( const MouseEvent& event ) override;
+    void mouseDrag( const MouseEvent& event ) override;
+    void mouseUp( const MouseEvent& event ) override;
 
 private:
     BaseComponent *owner;
-        
+    ScopedPointer<EditableTextObjListener> listener;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (EditableTextObj)
 
 };
@@ -63,45 +41,20 @@ private:
 class TextGlphComponent : public BaseComponent
 {
 public:
-    TextGlphComponent()
-    {
-        textobj = new EditableTextObj(this);
-        textobj->setText (m_text, sendNotificationSync );
-        textobj->setColour( Label::textColourId, getCurrentColor() );
-        textobj->setColour( Label::backgroundColourId, Colours::transparentWhite );
-        addAndMakeVisible(textobj);
+    TextGlphComponent();
+    ~TextGlphComponent();
+    
+    void setBoundsFromSymbol( float x, float y , float w , float h) override final;
+    
+    Rectangle<float> symbol_export_bounds() override;
+    
+    void importTextFromSymbol( const Symbol& s );
 
-    }
-    
-    ~TextGlphComponent(){
-        cout << "deleting text " <<  this << endl;
-    };
-    
-
-    void setBoundsFromSymbol( float x, float y , float w , float h) override final
-    {
-        //setBounds( x, y - (h * 0.5), w , h);
-        textobj->setFont( Font(h) );
-        setBounds( x, y, textobj->getFont().getStringWidth(m_text), h );
-    }
-    
-    Rectangle<float> symbol_export_bounds() override
-    {
-        auto b = getBounds().toFloat();
-        return Rectangle<float>( b.getX(), b.getY() + b.getHeight()/2, b.getWidth(), b.getHeight() );
-    }
-    
-    void importTextFromSymbol( const Symbol& s ) {}
-
-    void resized() override
-    {
-        BaseComponent::resized();
-        int h = getHeight();
-        textobj->setFont( Font( h ) );
-        textobj->setBounds( 0, 0, textobj->getFont().getStringWidth(m_text), h );
-    }
+    void resized() override;
     
     String getSymbolTypeStr() const override { return "text"; }
+    
+    void updateText( String str);
 
 private:
     
